d4e4c410b1d53246e69f9045a3e79f3f4001c138
Tobias Erbshäußer <tobias.erbshaeusser@gmail.com>
2019-10-04 09:44:16 +0200

    Designed public api.

--- Include/utf8streams.hpp
+++ Include/utf8streams.hpp
@@ -1,3 +1,23 @@
 #pragma once
+#include <istream>
 
-namespace utf8streams {}
+namespace utf8streams {
+
+enum class Encoding { Unknown, Utf8, Utf16LE, Utf16BE, Utf32LE, Utf32BE };
+
+class UTF8IStream : public std::istream {
+private:
+  std::streambuf *buf;
+
+public:
+  explicit UTF8IStream(std::istream &&stream,
+                       Encoding sourceEncoding = Encoding::Unknown);
+
+  UTF8IStream(const UTF8IStream &) = delete;
+
+  ~UTF8IStream() override;
+
+  Encoding getEncoding() const;
+};
+
+} // namespace utf8streams
--- Source/utf8streams.cpp
+++ Source/utf8streams.cpp
@@ -1,3 +1,15 @@
 #include "utf8streams.hpp"
 
-namespace utf8streams {}
+namespace utf8streams {
+
+UTF8IStream::UTF8IStream(std::istream &&stream, Encoding sourceEncoding)
+    : buf(nullptr) {
+  (void)stream;
+  (void)sourceEncoding;
+}
+
+UTF8IStream::~UTF8IStream() { delete buf; }
+
+Encoding UTF8IStream::getEncoding() const { return Encoding::Unknown; }
+
+} // namespace utf8streams
