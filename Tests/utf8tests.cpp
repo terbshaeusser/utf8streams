5f5641afb5d64f043a837ad9c008ad47979145a5
Tobias Erbshäußer <tobias.erbshaeusser@gmail.com>
2019-10-04 12:51:55 +0200

    Implemented the streambuf class.

--- Tests/utf8tests.cpp
+++ Tests/utf8tests.cpp
@@ -1,4 +1,14 @@
+#include <cstring>
 #include <gtest/gtest.h>
 #include <utf8streams.hpp>
 
-TEST(Dummy, test) { EXPECT_TRUE(true); }
+TEST(Utf8, ascii) {
+  std::istringstream stream("Hello World");
+  utf8streams::UTF8StreamBuf streamBuf(stream, utf8streams::Encoding::Utf8);
+
+  char buffer[128];
+  stream.read(buffer, sizeof(buffer));
+
+  EXPECT_EQ(11, stream.gcount());
+  EXPECT_EQ(0, std::memcmp("Hello World", buffer, 11));
+}
